eedd6eb079740066f6fae5b26b5400df99290a61
yymin1022 <yymin1022@gmail.com>
2021-07-01 15:35:46 +0000

    Update Recommendation.py : Apply NEW Code

--- Recommendation.py
+++ Recommendation.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[19]:
+# In[5]:
 
 
 import pandas as pd
 
 
-# In[20]:
+# In[6]:
 
 
 df = pd.read_excel("data.xlsx")
 df
 
 
-# In[21]:
+# In[7]:
 
 
 df['표준산업분류명'].value_counts()
 
 
-# In[22]:
+# In[8]:
 
 
 from geopy import distance
@@ -44,7 +44,7 @@ def dist_calc (df):
 df['distance3'] = df.apply (lambda df: dist_calc (df), axis=1)
 
 
-# In[23]:
+# In[9]:
 
 
 df_food = df[(df['표준산업분류명'] == '한식 음식점업') | (df['표준산업분류명'] == '분식 및 김밥 전문점') | (df['표준산업분류명'] == '서양식 음식점업')
@@ -53,26 +53,26 @@ df_food = df[(df['표준산업분류명'] == '한식 음식점업') | (df['표�
              | (df['표준산업분류명'] == '치킨 전문점')]
 
 
-# In[24]:
+# In[10]:
 
 
 df_cafe = df[(df['표준산업분류명'] == '비알콜 음료점업') | (df['표준산업분류명'] == '제과점업')]
 
 
-# In[25]:
+# In[11]:
 
 
 df_alcohol = df[(df['표준산업분류명'] == '기타 주점업') | (df['표준산업분류명'] == '일반유흥 주점업')]
 
 
-# In[26]:
+# In[12]:
 
 
 #longtitude = df['경도']
 #latitude = df['위도']
 
 
-# In[27]:
+# In[13]:
 
 
 food1 = df_food[df_food.distance1 < 500]
@@ -88,7 +88,7 @@ alcohol2 = df_alcohol[df_alcohol.distance2 < 500]
 alcohol3 = df_alcohol[df_alcohol.distance3 < 500]
 
 
-# In[28]:
+# In[14]:
 
 
 import folium
@@ -96,131 +96,242 @@ from folium.plugins import MarkerCluster
 
 
 def mapping(x):
-  map = folium.Map(location = [37.50566653486357, 126.9571440782172], zoom_start = 17)
-  list1=[]
-  list2=[]
-  marker_cluster = MarkerCluster().add_to(map)
+    map = folium.Map(location = [37.50566653486357, 126.9571440782172], zoom_start = 17)
+    list1=[]
+    list2=[]
+    marker_cluster = MarkerCluster().add_to(map)
 
-  for a in x.index:
-      folium.Marker(location = [x.loc[a,"위도"],x.loc[a,"경도"]],
+    for a in x.index:
+        folium.Marker(location = [x.loc[a,"위도"],x.loc[a,"경도"]],
                   popup=x.loc[a,"상호명"]).add_to(marker_cluster)
-      list1.append(x.loc[a,"위도"])
-      list2.append(x.loc[a,"경도"])
-  return map
+        list1.append(x.loc[a,"위도"])
+        list2.append(x.loc[a,"경도"])
+    
+    return map
 
 
-# In[29]:
+# In[68]:
 
 
 mapping(food1)
 
 
-# In[30]:
+# In[69]:
 
 
 mapping(food2)
 
 
-# In[31]:
+# In[70]:
 
 
 mapping(food3)
 
 
-# In[32]:
+# In[71]:
 
 
 mapping(cafe1)
 
 
-# In[33]:
+# In[72]:
 
 
 mapping(cafe2)
 
 
-# In[34]:
+# In[73]:
 
 
 mapping(cafe3)
 
 
-# In[35]:
+# In[74]:
 
 
 mapping(alcohol1)
 
 
-# In[36]:
+# In[75]:
 
 
 mapping(alcohol2)
 
 
-# In[37]:
+# In[76]:
 
 
 mapping(alcohol3)
 
 
-# In[38]:
+# In[101]:
 
-def getResult(where, what):
-    strResult = ""
 
-    if where == "정문":
-        if what == "음식점":
-            strResult = mapping(food1)
-        elif what == "카페":
-            strResult = mapping(cafe1)
-        elif what == "주점":
-            strResult = mapping(alcohol1)
-    elif where == "중문":
-        if what == "음식점":
-            strResult = mapping(food2)
-        elif what == "카페":
-            strResult = mapping(cafe2)
-        elif what == "주점":
-            strResult = mapping(alcohol2)
-    elif where == "후문":
-        if what == "음식점":
-            strResult = mapping(food3)
-        elif what == "카페":
-            strResult = mapping(cafe3)
-        elif what == "주점":
-            strResult = mapping(alcohol3)
-    
-    return f'정문주변주점을 찾은 결과입니다 {mapping(alcohol1)}'
+from PIL import Image
+ 
+image = Image.open("figure_food1.png")
+showimage1 = image.show()
+showimage1
+
+
+# In[98]:
+
+
+image = Image.open("figure_food2.png")
+showimage2 = image.show()
+showimage2
+
+
+# In[85]:
+
+
+image = Image.open("figure_food3.png")
+showimage3 = image.show()
+showimage3
+
+
+# In[86]:
+
+
+image = Image.open("figure_cafe1.png")
+showimage4 = image.show()
+showimage4
+
+
+# In[87]:
+
+
+image = Image.open("figure_cafe2.png")
+showimage5 = image.show()
+showimage5
+
+
+# In[97]:
+
+
+image = Image.open("figure_cafe3.png")
+showimage6 = image.show()
+showimage6
+
+
+# In[89]:
+
+
+image = Image.open("figure_alcohol1.png")
+showimage7 = image.show()
+showimage7
+
+
+# In[90]:
+
+
+image = Image.open("figure_alcohol2.png")
+showimage8 = image.show()
+showimage8
+
+
+# In[96]:
+
+
+image = Image.open("figure_alcohol3.png")
+showimage9 = image.show()
+showimage9
+
+
+# In[52]:
+
+
+food1 = food1.sort_values('distance1')
+food1
+
+
+# In[18]:
+
+
+food2 = food2.sort_values('distance2')
+food2
+
+
+# In[53]:
+
+
+food3 = food3.sort_values('distance3')
+food3
+
+
+# In[55]:
+
+
+cafe1 = cafe1.sort_values('distance1')
+cafe1
+
+
+# In[56]:
+
+
+cafe2 = cafe2.sort_values('distance2')
+cafe2
+
+
+# In[57]:
+
+
+cafe3 = cafe3.sort_values('distance3')
+cafe3
+
+
+# In[58]:
+
+
+alcohol1 = alcohol1.sort_values('distance1')
+alcohol1
+
+
+# In[59]:
+
+
+alcohol2 = alcohol2.sort_values('distance2')
+alcohol2 
+
+
+# In[60]:
+
+
+alcohol3 = alcohol3.sort_values('distance3')
+alcohol3
+
+
+# In[102]:
 
 
 async def 정문주변음식점(ctx):
-    await ctx.send(f'정문주변음식점을 찾은 결과입니다 {mapping(food1)}')
+    await ctx.send(f'정문주변음식점을 찾은 결과입니다 {food1},{showimage1}')
 
 async def 정문주변카페(ctx):
-    await ctx.send(f'정문주변카페를 찾은 결과입니다 {mapping(cafe1)}')
+    await ctx.send(f'정문주변카페를 찾은 결과입니다 {cafe1},{showimage4}')
     
 async def 정문주변주점(ctx):
-    await ctx.send(f'정문주변주점을 찾은 결과입니다 {mapping(alcohol1)}')
+    await ctx.send(f'정문주변주점을 찾은 결과입니다 {alcohol1},{showimage7}')
     
 
 async def 중문주변음식점(ctx):
-    await ctx.send(f'정문주변음식점을 찾은 결과입니다 {mapping(food2)}')
+    await ctx.send(f'중문주변음식점을 찾은 결과입니다 {food2},{showimage2}')
     
 async def 중문주변카페(ctx):
-    await ctx.send(f'정문주변카페를 찾은 결과입니다 {mapping(cafe2)}')
+    await ctx.send(f'중문주변카페를 찾은 결과입니다 {cafe2},{showimage5}')
     
 async def 중문주변주점(ctx):
-    await ctx.send(f'정문주변주점을 찾은 결과입니다 {mapping(alcohol2)}')
+    await ctx.send(f'중문주변주점을 찾은 결과입니다 {alcohol2},{showimage8}')
     
     
 async def 후문주변음식점(ctx):
-    await ctx.send(f'정문주변음식점을 찾은 결과입니다 {mapping(food3)}')
+    await ctx.send(f'후문주변음식점을 찾은 결과입니다 {food3},{showimage3}')
     
 async def 후문주변카페(ctx):
-    await ctx.send(f'정문주변카페를 찾은 결과입니다 {mapping(cafe3)}')
+    await ctx.send(f'후문주변카페를 찾은 결과입니다 {cafe3},{showimage6}')
     
 async def 후문주변주점(ctx):
-    await ctx.send(f'정문주변주점을 찾은 결과입니다 {mapping(alcohol3)}')
+    await ctx.send(f'후문주변주점을 찾은 결과입니다 {alcohol3},{showimage9}')
 
 
 # In[ ]:
